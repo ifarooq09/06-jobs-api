793610f4d676a815c70edb4d8dc75256f93ec14c
Ibrahim <ifarooq9@gmail.com>
2024-09-02 13:56:43 +0430

    Assignment for week 12 completed and submitting for review

--- app.js
+++ app.js
@@ -30,12 +30,16 @@ app.use(
     max: 100,
   })
 )
+
 app.use(express.json());
 app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
+// app.get('/', (req, res) => {
+//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+// });
+app.use(express.static("public"));
 
 //routes
 app.use('/api/v1/auth', authRouter)
